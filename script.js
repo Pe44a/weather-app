68233be1a8f4664180a609c15cf343cf31a74d03
Peteris <blankfeldspeteris@gmail.com>
2023-03-11 15:46:15 +0200

    Displays wether data

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const form = document.querySelector('.form');
 
 async function getWeatherData(location) {
     try{
-        const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=1b3886093d5b88fce06ead4d8437b018`);
+        const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=1b3886093d5b88fce06ead4d8437b018&units=metric`);
         const weather = await weatherData.json();
+        
         const data = {
             city: weather.name,
             temperature: weather.main.temp,
@@ -12,17 +13,35 @@ async function getWeatherData(location) {
             humidity: weather.main.humidity,
             howClearIsTheSky: weather.weather[0].main
         };
-        
-        console.log(data);
+
+        return data;
+
     } catch(error) {
         console.log(error);
     }
 };
+
+async function displayWetaherData (data){
+    const cityDiv = document.querySelector('.city');
+    const temperatureDiv = document.querySelector('.temperature');
+    const feelsLikeDiv = document.querySelector('.feelsLike');
+    const humidityDiv = document.querySelector('.humidity');
+    const windSpeedDiv = document.querySelector('.windSpeed');
+
+    // Waits
+    const recivedData = await data;
+
+    cityDiv.textContent = recivedData.city;   
+    temperatureDiv.textContent = recivedData.temperature;
+    feelsLikeDiv.textContent = recivedData.feelsLike;
+    humidityDiv.textContent = recivedData.humidity;
+    windSpeedDiv.textContent = recivedData.windSpeed;
+}
 
 
 form.addEventListener('submit',(e)=> {
     e.preventDefault();
     const city = document.querySelector('#searchCity').value;
 
-    getWeatherData(city);
+    displayWetaherData(getWeatherData(city));
 });
