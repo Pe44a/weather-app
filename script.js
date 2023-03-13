ae3a06f02a79ecdd901a5d0df351e4c2c208bb24
Peteris <blankfeldspeteris@gmail.com>
2023-03-13 23:04:22 +0200

    Styled & displays weather icon based on condition

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ async function getWeatherData(location) {
     try{
         const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=1b3886093d5b88fce06ead4d8437b018&units=metric`);
         const weather = await weatherData.json();
+        console.log(weather);
         
         const data = {
             city: weather.name,
@@ -11,9 +12,10 @@ async function getWeatherData(location) {
             feelsLike: weather.main.feels_like, 
             windSpeed: weather.wind.speed,
             humidity: weather.main.humidity,
-            howClearIsTheSky: weather.weather[0].main
+            howClearIsTheSky: weather.weather[0].description
         };
 
+        console.log(data);
         return data;
 
     } catch(error) {
@@ -21,7 +23,7 @@ async function getWeatherData(location) {
     }
 };
 
-async function displayWetaherData (data){
+async function displayWeatherData (data){
     const cityDiv = document.querySelector('.city');
     const temperatureDiv = document.querySelector('.temperature');
     const feelsLikeDiv = document.querySelector('.feelsLike');
@@ -29,19 +31,65 @@ async function displayWetaherData (data){
     const windSpeedDiv = document.querySelector('.windSpeed');
 
     // Waits
-    const recivedData = await data;
+    const receivedData = await data;
 
-    cityDiv.textContent = recivedData.city;   
-    temperatureDiv.textContent = recivedData.temperature;
-    feelsLikeDiv.textContent = recivedData.feelsLike;
-    humidityDiv.textContent = recivedData.humidity;
-    windSpeedDiv.textContent = recivedData.windSpeed;
+    cityDiv.textContent = receivedData.city;   
+    temperatureDiv.textContent = receivedData.temperature + '°C';
+    feelsLikeDiv.textContent = receivedData.feelsLike + '°C';
+    humidityDiv.textContent = receivedData.humidity + '%';
+    windSpeedDiv.textContent = receivedData.windSpeed + ' km/h';
 }
+
+// Displays 
+async function displayWeatherIcon (data) {
+    const receivedData = await data;
+    const image = document.querySelector('.image');
+
+    console.log(receivedData.howClearIsTheSky);
+
+    
+    switch (receivedData.howClearIsTheSky) {
+        case 'clear sky':
+            image.setAttribute('src', 'images/01d@2x.png.crdownload');
+            break;
+        case 'few clouds':
+            image.setAttribute('src', 'images/02d@2x.png.crdownload');
+            break;
+        case 'scattered clouds':
+            image.setAttribute('src', 'images/03d@2x.png.crdownload');
+            break;
+        case 'broken clouds':
+            image.setAttribute('src', 'images/04d@2x.png.crdownload');
+            break;
+        case 'shower rain':
+            image.setAttribute('src', 'images/09d@2x.png.crdownload');
+            break;
+        case 'rain':
+            image.setAttribute('src', 'images/10d@2x.png.crdownload');
+            break;
+
+        case 'thunderstorm':
+            image.setAttribute('src', 'images/11d@2x.png.crdownload');
+            break;
+        case 'snow':
+            image.setAttribute('src', 'images/13d@2x.png.crdownload');
+            break;
+        case 'mist':
+            image.setAttribute('src', 'images//50d@2x.png.crdownload');
+            break;
+
+        default:
+            image.setAttribute('src', 'images//50d@2x.png.crdownload');
+            break;
+    }
+
+};
 
 
 form.addEventListener('submit',(e)=> {
     e.preventDefault();
-    const city = document.querySelector('#searchCity').value;
 
-    displayWetaherData(getWeatherData(city));
+    const city = document.querySelector('#searchCity').value;
+    displayWeatherData(getWeatherData(city));
+    displayWeatherIcon(getWeatherData(city));
 });
